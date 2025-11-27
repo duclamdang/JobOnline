e30b5687c92c63e382ecc0fd42003cd06152c539
Đức Lâm <104142849+duclamdang@users.noreply.github.com>
2025-11-27 11:41:17 +0700

    fix_router_home

--- admin_client/src/routes.tsx
+++ admin_client/src/routes.tsx
@@ -1,7 +1,11 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import adminRoutes from "@admin/routes/admin.route";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/admin" replace />,
+  },
   {
     path: "/admin",
     children: adminRoutes,
